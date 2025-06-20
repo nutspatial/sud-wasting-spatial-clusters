211cd355c317436589114fda00f9f8db8f9899c4
tomaszaba <tomas.zaba@outlook.com>
2025-06-20 23:39:58 +0200

    update code to address #8

--- scripts/satscan.R
+++ scripts/satscan.R
@@ -7,27 +7,15 @@
 
 
 ## ---- Create a temporary directory -------------------------------------------
+
 destfile <- "data/satscan"
 filename <- "elfasher"
-
-
-## ---- Set SatScan analysis parameters ----------------------------------------
-
-do.call(
-  what = invisible,
-  args = list(
-    do.call(
-      what = ss.options,
-      args = list(
-        reset = TRUE
-      )
-    )
-  )
-)
+startdate <- "2025/06/20"
+enddate <- "2025/06/21"
 
 ## ---- Run SaTScan ------------------------------------------------------------
 
-do.call(
+results <- do.call(
   what = run_satscan,
   args = list(
     .data = smart_wfhz,
@@ -36,6 +24,11 @@ do.call(
     destfile_params = destfile,
     .scan_for = "high-rates", 
     verbose = TRUE, 
-    sslocation = "/Applications/SaTScan.app/Contents/app/satscan"
+    sslocation = "/Applications/SaTScan.app/Contents/app", 
+    satscan_version = "10.3.2", 
+    startdate = startdate,
+    enddate = enddate
   )
 )
+
+# =============================== End of Worflow ===============================
